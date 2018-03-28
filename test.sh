669a53a6a9dabded7697d1587899b135aea9f0e6
Victor Engmark <victor.engmark@gmail.com>
2018-03-28 20:25:35 +1300

    Use Arch Linux standard path for shunit2

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@
 #    https://github.com/l0b0/difff/issues
 #
 # COPYRIGHT AND LICENSE
-#    Copyright (C) 2011-2012 Victor Engmark
+#    Copyright (C) 2011-2018 Victor Engmark
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -132,4 +132,4 @@ test_fields_diff() {
 
 # load and run shUnit2
 test -n "${ZSH_VERSION:-}" && SHUNIT_PARENT=$0
-. /usr/share/shunit2/shunit2
+. /usr/bin/shunit2
